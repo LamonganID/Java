0fddb17c5f699031240450178c7e8a9605ed5750
Your Name <you@example.com>
2025-06-17 20:14:19 +0700

    Add salary calculation program and initialize coordinate quadrant program

--- Prak bab 2/soalprak2_2.java
+++ Prak bab 2/soalprak2_2.java
@@ -5,4 +5,20 @@ public class soalprak2_2 {
     // perjamnya dibayar Rp 7500/jam. Tetapi jika jam kerjanya kurang dari 50 jam/minggu,
     // maka dikenakan potongan Rp.2500/jam nya. (Input : jam kerja/minggu, dan output : total
     // gaji)
+    public static void main(String[] args) {
+        java.util.Scanner masukan = new java.util.Scanner(System.in);
+        int jamKerja;
+        System.out.print("Masukkan jam kerja karyawan (jam/minggu) = ");
+        jamKerja = masukan.nextInt();
+        int gaji;
+        if (jamKerja >= 50 && jamKerja <= 60) {
+            gaji = jamKerja * 5000;
+        } else if (jamKerja > 60) {
+            gaji = (50 * 5000) + ((jamKerja - 50) * 7500);
+        } else {
+            gaji = jamKerja * 5000 - (jamKerja * 2500);
+        }
+        System.out.printf("Total gaji karyawan = Rp.%,d\n", gaji);
+        masukan.close();
+    }
 }
--- Prak bab 2/soalprak2_3.java
+++ Prak bab 2/soalprak2_3.java
@@ -2,4 +2,7 @@ public class soalprak2_3 {
     //Buat program untuk menentukan kuadran dari suatu titik koordinat. Nilai inputnya
     // adalah nilai X dan Y suatu titik, dan outputnya adalah nomor kuadran dari koordinat
     // tersebut
+    public static void main(String[] args) {
+        
+    }
 }
