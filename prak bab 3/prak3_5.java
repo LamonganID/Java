07ce194350539d49515d821332c861240c8a60c2
Your Name <you@example.com>
2025-06-18 20:43:19 +0700

    Refactor main method in prak3_5 class to improve structure and readability

--- prak bab 3/prak3_5.java
+++ prak bab 3/prak3_5.java
@@ -1,4 +1,16 @@
 public class prak3_5 {
-    
-
+    public static void main(String[] args) {
+        java.util.Scanner masukan = new java.util.Scanner(System.in);
+        int n=0, total = 0, bil=9;
+        while (bil!=0){
+            n++;
+            System.out.println("MAsukan bilangan ke "+-n+" =");
+            bil = masukan.nextInt();
+            total += bil;
+        }
+        // Mengurangi 1 karena bilangan terakhir yang dimasukkan adalah 0
+        System.out.println("Total Jumlah "+(n-1)+ "bilangan tersebut = ");
+        System.out.println(total);
+        masukan.close();
+    }
 }
